c3e83f64bfd0bc647a4d21104fd2b2bea339df1f
endriu <andreagreco2@gmail.com>
2024-07-09 17:59:12 +0200

    metadata seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Metadata } from 'next';
+ 
 
 export const experimental_ppr = true; // to implement Partial Prerendering PPR
-
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
   children,
 }: {
